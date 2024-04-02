659b5434264899b3166b41caca81fde09c9b523e
Bhanuteja005 <pashikantibhanuteja@gmail.com>
2024-04-02 22:15:18 +0530

    bhanu

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -5,26 +5,24 @@ import Image from "next/image";
 export default function TracingBeamDemo() {
   return (
     <TracingBeam className="px-6 mt-32">
-        <h2 className="text-2xl md:text-4xl lg:text-7xl text-white font-bold text-center mb-8">About us</h2>
+      <h2 className="text-2xl md:text-4xl lg:text-7xl text-white font-bold text-center mb-8">About us</h2>
       <div className="max-w-2xl mx-auto antialiased pt-4 relative">
         {dummyContent.map((item, index) => (
           <div key={`content-${index}`} className="mb-10">
             <h2 className="bg-black text-white rounded-full text-sm w-fit px-4 py-1 mb-4">
               {item.badge}
             </h2>
-
             <p className="text-xl mb-4" style={{ fontFamily: 'Calsans', fontWeight: 600 }}>
-  {item.title}
-</p>
-
-            <div className="text-sm  prose prose-sm dark:prose-invert">
-
+              {item.title}
+            </p>
+            <div className="text-sm prose prose-sm dark:prose-invert">
               {item?.image && (
                 <Image
                   src={item.image}
                   alt="blog thumbnail"
-                  height="1000"
-                  width="1000"
+                  height={500}
+                  width={500}
+                  layout="responsive"
                   className="rounded-lg mb-10 object-cover"
                 />
               )}
@@ -35,15 +33,13 @@ export default function TracingBeamDemo() {
       </div>
     </TracingBeam>
   );
-}
-
-const dummyContent = [
+}const dummyContent = [
   {
     title: "Our mission",
     description: (
       <>
         <p>
-        Welcome to AIM, where Aspiring Intellectual Mindsets converge to shape the future. AIM is not just an acronym, it's a commitment to pushing the boundaries of knowledge and creativity. In the dynamic landscape of AIM, we are not merely students, we are pioneers, visionaries, and architects of the digital age. Our journey is marked by a relentless pursuit of excellence, a passion for discovery, and a collective aspiration to make a meaningful impact.As we embark on this adventure, the possibilities are limitless, and the challenges are met with enthusiasm.
+        Welcome to AIM, where Aspiring Intellectual Mindsets converge to shape the future. AIM is not just an acronym, it&apos;s a commitment to pushing the boundaries of knowledge and creativity. In the dynamic landscape of AIM, we are not merely students, we are pioneers, visionaries, and architects of the digital age. Our journey is marked by a relentless pursuit of excellence, a passion for discovery, and a collective aspiration to make a meaningful impact.As we embark on this adventure, the possibilities are limitless, and the challenges are met with enthusiasm.
         </p>
 
       </>
@@ -70,7 +66,7 @@ const dummyContent = [
     description: (
       <>
         <p>
-          At AIM Club, we believe in the power of curiosity and the thrill of discovery. We dive into the depths of artificial intelligence, exploring its vast potential and pushing the boundaries of what's possible. Our members are not just learners, they are explorers charting the uncharted territories of AI. We tackle challenges head-on, turning obstacles into opportunities for learning and growth. Join us as we embark on this exciting journey into the future of AI.
+          At AIM Club, we believe in the power of curiosity and the thrill of discovery. We dive into the depths of artificial intelligence, exploring its vast potential and pushing the boundaries of what&apos;s possible. Our members are not just learners, they are explorers charting the uncharted territories of AI. We tackle challenges head-on, turning obstacles into opportunities for learning and growth. Join us as we embark on this exciting journey into the future of AI.
         </p>
       </>
     ),
