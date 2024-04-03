03ad2e2a023473a2f1f813cc4ef5914619cc1af6
Bhanuteja005 <pashikantibhanuteja@gmail.com>
2024-04-03 14:17:31 +0530

    aim

--- src/app/events/page.tsx
+++ src/app/events/page.tsx
@@ -11,7 +11,6 @@ export default function TracingBeamDemo() {
         {dummyContent.map((item, index) => (
           <div key={`content-${index}`} className="mb-10">
             <h2 className="bg-black text-white rounded-full text-sm w-fit px-4 py-1 mb-4">
-              {item.badge}
             </h2>
             <p className="text-xl mb-4" style={{ fontFamily: 'Calsans', fontWeight: 600 }}>
               {item.title}
@@ -52,7 +51,7 @@ const dummyContent = [
         </p>
         <p>
         The program is aimed at MLRIT students, requiring no prerequisites. Whether you have a specific 
-        idea or not, we're here to guide you through the exciting journey of ideation in the realm of technology.
+        idea or not, we&apos;re here to guide you through the exciting journey of ideation in the realm of technology.
          With just a dash of enthusiasm, you can embark on an effortless incubation experience. Gain valuable
           insights by connecting with faculty, alumni startups, and field experts. Explore the intricacies of 
           the startup market and culture, making the Immersive program the perfect launchpad for your entrepreneurial aspirations.
@@ -60,7 +59,6 @@ const dummyContent = [
 
       </>
     ),
-    badge: "",
     image:
       "https://i.pinimg.com/736x/50/f7/04/50f704964d9ff70d382fb35b1a6ce3a9.jpg",
   },
@@ -69,18 +67,17 @@ const dummyContent = [
     description: (
       <>
          <p>
-          "Squid Game" was an exciting event organized by our college's AIM Club. 
-          This unique programming competition was inspired by the popular TV series "Squid Game".
+          &quot;Squid Game&quot; was an exciting event organized by our college&apos;s AIM Club. 
+          This unique programming competition was inspired by the popular TV series &quot;Squid Game&quot;.
            Participants were challenged to solve complex coding problems under time pressure,
             mimicking the high-stakes environment of the show. The event was not only a test of coding skills 
             but also of strategy, teamwork, and resilience. It was a thrilling experience that brought together
              coding enthusiasts from all disciplines, fostering a spirit of camaraderie and friendly competition. 
-             The "Squid Game" event was a testament to the innovative and engaging activities that
+             The &quot;Squid Game&quot; event was a testament to the innovative and engaging activities that
               AIM Club continually brings to our college community.
         </p>
       </>
     ),
-    badge: "",
     image:
       "https://i.pinimg.com/564x/9b/ff/4a/9bff4a63e285fba08fb6dd7cc28f8d40.jpg",
   },
@@ -89,7 +86,7 @@ const dummyContent = [
     description: (
       <>
         <p>
-          "Gen AI" was a landmark event hosted by AIM Club at our college. This event was designed to inspire and
+          &quot;Gen AI&quot; was a landmark event hosted by AIM Club at our college. This event was designed to inspire and
            educate students about the vast potential of artificial intelligence. It featured a series of interactive
             workshops, enlightening talks by industry experts, and hands-on coding sessions. Participants had the 
             opportunity to delve into various AI technologies, work on real-world projects, and interact with AI 
