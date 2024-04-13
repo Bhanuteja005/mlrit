5cb929cb71522f8854896de42a3f07c2de58468f
Bhanuteja005 <pashikantibhanuteja@gmail.com>
2024-04-13 12:25:41 +0530

    aim

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,27 +5,13 @@ import GlobePage from "@/components/GlobePage";
 import HeroSection from "@/components/HeroSection";
 import ImageSlider from "@/components/ImageSLider";
 import Instructors from "@/components/Instructors";
-import MultiStepLoaderDemo from "@/components/Preloader";
 import WhyChooseUs from "@/components/WhyChooseUs";
 import AimTestimonials from "@/components/ui/TestimonialCards";
-import { useEffect, useState } from 'react';
 
 export default function Home() {
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      setLoading(false);
-    }, 5000); // Change this to control how long the preloader is displayed
-
-    return () => clearTimeout(timer);
-  }, []);
 
   return (
     <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
-      {loading ? (
-        <MultiStepLoaderDemo />
-      ) : (
         <>
           <HeroSection/>
           <About/>
@@ -36,7 +22,6 @@ export default function Home() {
           <ImageSlider/>
           <Footer/>
         </>
-      )}
     </main>
   );
 }
